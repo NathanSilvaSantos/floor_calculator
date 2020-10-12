4e99c446c4d3d33edaf6f7568520deb14579443f
NathanSilvaSantos <nathan013silva@gmail.com>
2020-10-11 23:02:24 -0300

    Finalizando

---
+++ lib/core/theme_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final themeApp = ThemeData(
+  brightness: Brightness.light,
+  primarySwatch: Colors.deepPurple,
+  accentColor: Colors.deepPurpleAccent,
+  buttonTheme: ButtonThemeData(
+    height: 52,
+    textTheme: ButtonTextTheme.primary
+  ),
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
+import 'package:floor_calculator/core/theme_app.dart';
+import 'package:floor_calculator/pages/calculator_page.dart';
 import 'package:flutter/material.dart';
 
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  title: "Calculadora de pisos",
+  theme: themeApp,
+  home: CalculatorPage(),
+));
 
 
